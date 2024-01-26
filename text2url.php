90455c4e40ec2dba3c4d718108d8b89113cb5c56
ok2nd <m.ok.2nd@gmail.com>
2024-01-26 17:21:53 +0900

    2024.1.26

--- text2url.php
+++ text2url.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Real-time URL Parameter Update</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>HTMLテキストボックスに入力されている文字列をリアルタイムにURLのパラメーターに反映する方法</title>
 </head>
 <body>
 <h4>
